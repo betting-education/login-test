f746e72f32b0d7e635120edfc7e5c3738b20f218
frederico-betting <frederico.betting@gmail.com>
2023-01-12 19:58:22 -0300

    Update login.js

--- docs/login.js
+++ docs/login.js
@@ -3,14 +3,15 @@ $(function () {
     $("#signIn").on("click", function() {
         var username = $("#user").val();
         var password = $("#password").val();
-
+        
+        $("#message > span">.remove()
+        
         if (username === "user@email.com" && password === "password") {
-            // If the credentials are valid, show an alert box and reload the page
-            alert("You have successfully logged in.");
-            location.reload();
+            window.location.assign("done.html");
+        } else if (username === "" && password === "") {
+            $("<span id=\"empty_field_msg\"><p>Please, provide username and password!</p></span>").appendTo("#message");
         } else {
-            // Otherwise, make the login error message show (change its oppacity)
-            $("<p>Invalid username and/or password!</p>").appendTo("#message");
+            $("<span id=\"error_msg\"><p>Invalid username and/or password!</p></span>").appendTo("#message");
         }
     });
 });
