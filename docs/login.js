ae5f0e8c2667cd871e41c52427e2d4819f32ecf4
frederico-betting <frederico.betting@gmail.com>
2023-01-13 08:48:27 -0300

    Update login.js

--- docs/login.js
+++ docs/login.js
@@ -8,7 +8,7 @@ $(function () {
         if (username === "user@email.com" && password === "password") {
             window.location.assign("done.html");
         } else if (username === "" && password === "") {
-            $("<span id=\"empty-field-msg\ class=\"error\"><p>Please, provide username and password!</p></span>").appendTo("#message");
+            $("<span id=\"empty-field-msg\" class=\"error\"><p>Please, provide username and password!</p></span>").appendTo("#message");
         } else {
             $("<span id=\"error-msg\" class=\"error\"><p>Invalid username and/or password!</p></span>").appendTo("#message");
         }
